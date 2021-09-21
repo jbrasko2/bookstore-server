5f74bea2d9017ecc8f674c9ce0e5d38c52425ae0
jbrasko <jbrasko0508@gmail.com>
2021-09-21 13:57:15 -0400

    build post route

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -9,8 +9,22 @@ router.get('/authors', (req, res) => {
   res.send('Authors page')
 })
 
-router.post('/authors', (req, res) => {
-  res.send(req.body)
+router.post('/authors', async (req, res) => {
+  const { name, dob } = req.body
+
+  if (!name || !dob) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and date of birth' })
+  }
+
+  try {
+    const author = new Author({ name, dob })
+    await author.save()
+    res.send(author)
+  } catch (err) {
+    res.status(422).send({ error: err.message })
+  }
 })
 
 module.exports = router
