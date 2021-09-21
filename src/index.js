66a36df6acffff6f4edeeaa4d130f2a2142d52aa
jbrasko <jbrasko0508@gmail.com>
2021-09-21 11:49:47 -0400

    add start script, connect to mongoDB

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+const mongoUri = process.env.DB_CONNECTION
+
+mongoose.connect(mongoUri)
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', err => {
+  console.log('Error connection to mongo', err)
+})
+
 app.get('/', (req, res) => {
   res.send('Hello')
 })
