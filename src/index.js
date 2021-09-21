55fa91afe9616c992dc27c40e8c0025262564583
jbrasko <jbrasko0508@gmail.com>
2021-09-21 11:47:20 -0400

    add first get, listen on 3000

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on Port 3000')
+})
