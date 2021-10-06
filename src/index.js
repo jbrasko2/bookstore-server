6133973517462603574392a0b4f522b2fc90885c
jbrasko <jbrasko0508@gmail.com>
2021-10-06 15:59:57 -0400

    update port connection

--- src/index.js
+++ src/index.js
@@ -27,6 +27,8 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log('Listening on Port 3000');
 });
